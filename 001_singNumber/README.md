66a8ca001edc23abd8db75eb84712edab234e05c
jieshi <jieshi@localhost.localdomain>
2017-03-12 10:00:18 +0800

    leetcode

---
+++ 001_singNumber/README.md
@@ -0,0 +1,5 @@
+Question:
+    Given an array integers , every element appears twice except ont , find that single one
+
+Solution:
+    using operator '^' . cause two same number ^ equal zero . and the last exist a number don't equal zero , that the single one
---
+++ 001_singNumber/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int SingleNumber(const vector<int> &nums)
+    {
+        int singleDog = 0;
+        for(auto it = nums.begin(); it != nums.end(); ++it)
+        {
+            singleDog ^= *it;
+        }
+        return singleDog;
+    }
+};
+
+int main()
+{
+    vector<int> nums{1,1,2,2,3,4,4,5,6,6,5};
+    Solution obj;
+    cout<<obj.SingleNumber(nums)<<endl;
+    return 0;
+}
