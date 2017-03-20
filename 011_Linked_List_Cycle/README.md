7a8bd9ac4a21880b05ba5f5953df602b24835240
jieshicheng <951497836@qq.com>
2017-03-20 19:36:59 +0800

    leetcode

--- 011_Linked_List_Cycle/README.md
+++ 011_Linked_List_Cycle/README.md
@@ -3,4 +3,4 @@ Question:
 	you can't use extra space
 
 Solution:
-	
+	like two person running,if here has a cycle,run faster on must can catch the slower one.
--- 011_Linked_List_Cycle/main.cpp
+++ 011_Linked_List_Cycle/main.cpp
@@ -2,10 +2,41 @@
 
 using namespace std;
 
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution
 {
 public:
-	
+	bool hasCycle(ListNode *head)
+	{
+		if(head && head->next)
+		{
+			bool flag = false;
+			ListNode *pre = head;
+			ListNode *suf = head->next;
+			while(suf)
+			{
+				if(suf == pre)
+					return true;
+				else
+				{
+					if(flag)
+					{
+						flag = !flag;
+						pre = pre->next;
+					}
+					suf = suf->next;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
 };
 
 int main()
