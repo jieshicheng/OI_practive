0b038a18e00bcc36c3d5835cbc6e479bcd98bf59
jieshicheng <951497836@qq.com>
2017-05-19 00:14:18 +0800

    DFS-Med-Leetcode

--- 063_Friend_Circle/main.cpp
+++ 063_Friend_Circle/main.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+class Solution
+{
+public:
+    void dfs(vector<vector<int>> &M, int beg)
+    {
+        for(size_t i = 0; i != M[beg].size(); ++i)
+        {
+            if(M[beg][i] == 1 && beg != i)
+            {
+                M[beg][i] = 0;
+                M[i][beg] = 0;
+                M[i][i] = 0;
+                M[beg][beg] = 0;
+                dfs(M, i);
+            }
+        }
+        return ;
+    }
 
+    int findCircleNum(vector<vector<int>> &M)
+    {
+        int ans =  0;
+        for(size_t i = 0; i != M.size(); ++i)
+        {
+            for(size_t j = 0; j != M[i].size(); ++j)
+            {
+                if(M[i][j] == 1)
+                {
+                    ++ans;
+                    dfs(M, i);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+int main()
+{
+    return 0;
+}
