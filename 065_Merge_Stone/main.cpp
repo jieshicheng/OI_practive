4ec246c46c91c9528c54240ff6e15b1cbd898dfe
jieshicheng <951497836@qq.com>
2017-05-21 00:08:42 +0800

    DP-Hard-POJ

--- 065_Merge_Stone/main.cpp
+++ 065_Merge_Stone/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,29 +10,37 @@ public:
     {
         if(n == 0)
             return 0;
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<int>> dp(n, vector<int>(n, 0xfffff));
         vector<int> sum(n, 0);
         sum[0] = nums[0];
+        dp[0][0] = 0;
         for(size_t i = 1; i < n; ++i)
+        {
             sum[i] = sum[i - 1] + nums[i];
-
-        for(int step = 1; step < n; ++i)
+            dp[i][i] = 0;
+        }
+        for(int step = 1; step < n; ++step)
         {
             for(int i = 0; i + step < n; ++i)
             {
-                j = i + step;
+                int j = i + step;
                 for(int k = i; k < j; ++k)
                 {
-                    dp[i][j] = min(dp[i][k], dp[k + 1][j]);
+                    dp[i][j] = min(dp[i][k] + dp[k + 1][j], dp[i][j]);
                 }
                 dp[i][j] += sum[j] - sum[i] + nums[i];
             }
         }
+        return dp[0][n - 1];
     }
 };
 
 int main()
 {
+    int n = 7;
+    vector<int> nums{13,7,8,16,21,4,18};
+    Solution obj;
+    cout<<obj.mergeStone(n, nums)<<endl;
 	return 0;
 }
 
