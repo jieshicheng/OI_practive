61f8b6046f555803a6e66ca6004e545f4035f48c
jieshicheng <951497836@qq.com>
2017-12-18 00:28:29 +0800

    LeetCode Med

---
+++ 082_Container_with_Most_water/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int water = 0;
+        int i = 0, j = height.size() - 1;
+        while (i < j) {
+            int h = min(height[i], height[j]);
+            water = max(water, (j - i) * h);
+            while (height[i] <= h && i < j) i++;
+            while (height[j] <= h && i < j) j--;
+        }
+        return water;
+    }
+};
+
+int main() {
+	return 0;
+}
+
