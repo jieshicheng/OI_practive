23a02b5d022c2bec25b4c521e70d718bbc1e2135
jieshicheng <951497836@qq.com>
2018-01-22 23:52:48 +0800

    LeetCode Med Interesting Issue

---
+++ 086_FindDuplicateNumber/README.md
@@ -0,0 +1,5 @@
+LeetCode 287 Find the Duplicate Number
+
+Answer:
+    不会这道题，看了题解学会的快慢指针。很clever的一个想法。学习了
+    推荐不会的一定要理解这道题。很有价值。具体大家看LeetCode的Discuss即可。本人讲的不如他们好
---
+++ 086_FindDuplicateNumber/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    int findDuplicate3(vector<int>& nums)
+    {
+	    if (nums.size() > 1)
+	    {
+		    int slow = nums[0];
+		    int fast = nums[nums[0]];
+		    while (slow != fast)
+		    {
+			    slow = nums[slow];
+			    fast = nums[nums[fast]];
+		    }   
+            cout << slow << endl;
+		    fast = 0;
+		    while (fast != slow)
+		    {
+			    fast = nums[fast];
+			    slow = nums[slow];
+		    }
+		    return slow;
+	    }
+	    return -1;
+    }	
+};
+
+int main() {
+    Solution solv;
+    vector<int> vec{1,6,3,4,5,6,2};
+    cout << solv.findDuplicate3(vec);
+	return 0;
+}
+
