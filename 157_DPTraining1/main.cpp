895d3a5025967f2e6a49e765b3e136fd62a102fa
jieshicheng <951497836@qq.com>
2018-05-21 00:21:58 +0800

    ACM train DP issue

--- 157_DPTraining1/main.cpp
+++ 157_DPTraining1/main.cpp
@@ -13,13 +13,13 @@ using namespace std;
 
 class Solution {
 public:
-    int recur(const vector<vector<bool>> &graph, vector<int> &dp, i) {
+    int recur(const vector<vector<bool>> &graph, vector<int> &dp, int i) {
         if( dp[i] != -1 ) 
             return dp[i];
         dp[i] = 1;
         for( int j = 0; j != graph.size(); ++j ) {
             if( graph[i][j] ) {
-                dp[i] = max(recur(j) + 1, dp[i]);
+                dp[i] = max(recur(graph, dp, j) + 1, dp[i]);
             }
         }
         return dp[i];
@@ -34,7 +34,7 @@ public:
         vector<vector<bool>> graph = getGraph(matrix);
         vector<int> dp(size, -1);
         recur(graph, dp, 0);
-        int maxIndex = 0
+        int maxIndex = 0;
         for( int i = 1; i != size; ++i ) {
             if( recur(graph, dp, i) > dp[maxIndex] )
                 maxIndex = i;
@@ -45,17 +45,18 @@ public:
 
     void print_ans(const vector<int> &dp, vector<int> &ans, int i, const vector<vector<bool>> &graph) {
         ans.push_back(i);
-        for( int j = 0; j != size; ++j ) {
-            if( graph[i][j] && dp[i] == dp[j] + 1 ) 
-                print_ans;
+        for( int j = 0; j != graph.size(); ++j ) {
+            if( graph[i][j] && dp[i] == dp[j] + 1 ) {
+                print_ans(dp, ans, j, graph);
                 break;
+            }
         }
     }
 
     vector<vector<bool>> getGraph(const vector<pair<int, int>> &matrix) {
-        vector<vector<bool>> graph(size, vector<bool>(size, false));
-        for( int i = 0; i != size; ++i ) {
-            for( int j = 0; j != size; ++j ) {
+        vector<vector<bool>> graph(matrix.size(), vector<bool>(matrix.size(), false));
+        for( int i = 0; i != graph.size(); ++i ) {
+            for( int j = 0; j != graph.size(); ++j ) {
                 if( matrix[i].first < matrix[j].first && matrix[i].second < matrix[j].second ) {
                     graph[i][j] = true;
                     continue;
@@ -65,11 +66,21 @@ public:
                 }
             }
         }
+        /*
+        for( auto i : graph ) {
+            for( auto j : i )
+                cout << j << " ";
+            cout << endl;
+        }*/
         return graph;
     }
 };
 
 int main() {
-	return 0;
+	Solution solv;
+    vector<int> ans = solv.answer({{1,2}, {3,4}, {5,6}, {6,2}, {6,7}});
+    for( auto i : ans )
+        cout << i << endl;
+    return 0;
 }
 
